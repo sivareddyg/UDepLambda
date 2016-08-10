3c45d6b7c6d02a6b2c0c702ef8b0b088c811f2a0
Siva Reddy <siva@sivareddy.in>
2016-08-10 23:41:38 +0100

    utf8 reading

--- src/deplambda/others/MergeConllAndGraphParserFormats.java
+++ src/deplambda/others/MergeConllAndGraphParserFormats.java
@@ -1,11 +1,14 @@
 package deplambda.others;
 
 import java.io.BufferedReader;
+import java.io.BufferedWriter;
 import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
 import java.io.PrintStream;
+import java.io.Writer;
 
 import com.google.common.base.Preconditions;
 import com.google.gson.Gson;
@@ -19,10 +22,13 @@ public class MergeConllAndGraphParserFormats {
 
   public static void merge(InputStream conllInputStream,
       InputStream graphParserInputStream, PrintStream out) throws IOException {
+    Writer writer = new OutputStreamWriter(out, "UTF-8");
+    BufferedWriter fout = new BufferedWriter(writer);
     BufferedReader conllReader =
-        new BufferedReader(new InputStreamReader(conllInputStream));
-    BufferedReader graphParserReader =
-        new BufferedReader(new InputStreamReader(graphParserInputStream));
+        new BufferedReader(new InputStreamReader(conllInputStream, "UTF8"));
+    BufferedReader graphParserReader = new BufferedReader(
+        new InputStreamReader(graphParserInputStream, "UTF8"));
+
     String line = graphParserReader.readLine();
     while (line != null) {
       if (line.startsWith("#") || line.trim().equals("")) {
@@ -34,11 +40,13 @@ public class MergeConllAndGraphParserFormats {
         mergeForestWithConll(sentence, conllReader);
       else
         mergeSentenceWithConll(sentence, conllReader);
-      out.println(gson.toJson(sentence));
+      fout.write(gson.toJson(sentence));
+      fout.newLine();
       line = graphParserReader.readLine();
     }
     conllReader.close();
     graphParserReader.close();
+    fout.close();
   }
 
   public static void mergeForestWithConll(JsonObject forest,
@@ -55,13 +63,10 @@ public class MergeConllAndGraphParserFormats {
         conllSentence.get(SentenceKeys.WORDS_KEY).getAsJsonArray();
     JsonArray originalWords =
         sentence.get(SentenceKeys.WORDS_KEY).getAsJsonArray();
-    Preconditions
-        .checkArgument(
-            conllWords.size() == originalWords.size(),
-            String
-                .format(
-                    "Words in CONLL file and GraphParser file do not align\n CONLL: %s \n GraphParser: %s",
-                    conllWords, originalWords));
+    Preconditions.checkArgument(conllWords.size() == originalWords.size(),
+        String.format(
+            "Words in CONLL file and GraphParser file do not align\n CONLL: %s \n GraphParser: %s",
+            conllWords, originalWords));
     sentence.add(SentenceKeys.WORDS_KEY, conllWords);
   }
 
