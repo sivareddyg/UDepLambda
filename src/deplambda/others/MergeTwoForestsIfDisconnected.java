da994bf0341e095404d24b1c339d3c1bc6d96e4c
Siva Reddy <siva@sivareddy.in>
2016-12-20 12:18:18 +0000

    graph parser arg changes

--- src/deplambda/others/MergeTwoForestsIfDisconnected.java
+++ src/deplambda/others/MergeTwoForestsIfDisconnected.java
@@ -31,7 +31,7 @@ public class MergeTwoForestsIfDisconnected extends ProcessStreamInterface {
           false, false, false, false, false, false, false, false, false, false,
           false, false, false, false, false, false, false, false, false, false,
           false, false, false, false, false, false, false, false, false, false,
-          false, false, false, false, false, 10.0, 1.0, 0.0, 0.0, 0.0);
+          false, false, false, false, false, 10.0, 1.0, 0.0, 0.0, 0.0); 
     } catch (IOException e) {
       e.printStackTrace();
     }
