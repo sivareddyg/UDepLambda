2da5d7933cae7d309738043c892c8c1e3db7e476
Siva Reddy <siva@sivareddy.in>
2017-01-07 12:58:40 +0000

    sentence test update

--- test/deplambda/util/SentenceTest.java
+++ test/deplambda/util/SentenceTest.java
@@ -19,17 +19,13 @@ import deplambda.util.Sentence;
  */
 public class SentenceTest {
 
-  private JsonObject jsonSentence;
+  private static JsonParser json = new JsonParser();
 
   /**
    * @throws java.lang.Exception
    */
   @Before
-  public void setUp() throws Exception {
-    String dependencyTreeJson =
-        "{\"entities\": [{\"start\": 0, \"ner\": \"ORGANIZATION\", \"end\": 1, \"entity\": \"m.0k8z\"}, {\"start\": 3, \"ner\": \"PERSON\", \"end\": 4, \"entity\": \"m.06y3r\"}, {\"start\": 8, \"ner\": \"MISC\", \"end\": 9, \"entity\": \"m.04yg_s\"}], \"words\": [{\"word\":\"Apple\",\"head\":2,\"pos\":\"NNP\",\"dep\":\"nn\"}, {\"word\":\"Inc.\",\"head\":8,\"pos\":\"NNP\",\"dep\":\"nsubj\"}, {\"word\":\"which\",\"head\":6,\"pos\":\"WP\",\"dep\":\"dobj\"}, {\"word\":\"Steve\",\"head\":5,\"pos\":\"NNP\",\"dep\":\"nn\"}, {\"word\":\"Jobs\",\"head\":6,\"pos\":\"NNP\",\"dep\":\"nsubj\"}, {\"word\":\"founded\",\"head\":2,\"pos\":\"VBD\",\"dep\":\"rcmod\", \"lemma\":\"found\"}, {\"word\":\",\",\"head\":2,\"pos\":\".\",\"dep\":\"p\"}, {\"word\":\"developed\",\"head\":0,\"pos\":\"VBD\",\"dep\":\"ROOT\"}, {\"word\":\"iPod\",\"head\":10,\"pos\":\"NNP\",\"dep\":\"nn\"}, {\"word\":\"shuffle\",\"head\":8,\"pos\":\"NN\",\"dep\":\"dobj\"}, {\"word\":\".\",\"head\":8,\"pos\":\".\",\"dep\":\"p\"}]}";
-    JsonParser json = new JsonParser();
-    jsonSentence = json.parse(dependencyTreeJson).getAsJsonObject();
+  public void setUp() {
   }
 
   /**
@@ -38,9 +34,13 @@ public class SentenceTest {
    */
   @Test
   public void testSentence() {
+    JsonObject jsonSentence = json
+        .parse(
+            "{\"entities\": [{\"start\": 0, \"ner\": \"ORGANIZATION\", \"end\": 1, \"entity\": \"m.0k8z\"}, {\"start\": 3, \"ner\": \"PERSON\", \"end\": 4, \"entity\": \"m.06y3r\"}, {\"start\": 8, \"ner\": \"MISC\", \"end\": 9, \"entity\": \"m.04yg_s\"}], \"words\": [{\"word\":\"Apple\",\"head\":2,\"pos\":\"NNP\",\"dep\":\"nn\"}, {\"word\":\"Inc.\",\"head\":8,\"pos\":\"NNP\",\"dep\":\"nsubj\"}, {\"word\":\"which\",\"head\":6,\"pos\":\"WP\",\"dep\":\"dobj\"}, {\"word\":\"Steve\",\"head\":5,\"pos\":\"NNP\",\"dep\":\"nn\"}, {\"word\":\"Jobs\",\"head\":6,\"pos\":\"NNP\",\"dep\":\"nsubj\"}, {\"word\":\"founded\",\"head\":2,\"pos\":\"VBD\",\"dep\":\"rcmod\", \"lemma\":\"found\"}, {\"word\":\",\",\"head\":2,\"pos\":\".\",\"dep\":\"p\"}, {\"word\":\"developed\",\"head\":0,\"pos\":\"VBD\",\"dep\":\"ROOT\"}, {\"word\":\"iPod\",\"head\":10,\"pos\":\"NNP\",\"dep\":\"nn\"}, {\"word\":\"shuffle\",\"head\":8,\"pos\":\"NN\",\"dep\":\"dobj\"}, {\"word\":\".\",\"head\":8,\"pos\":\".\",\"dep\":\"p\"}]}")
+        .getAsJsonObject();
     Sentence sent = new Sentence(jsonSentence);
     assertEquals(
-        "(l-ROOT w-developed t-VBD (l-nsubj w-Inc. t-NNP (l-rcmod w-found t-VBD (l-dobj w-which t-WP) (l-nsubj w-Jobs t-NNP)) (l-p w-, t-.)) (l-dobj w-shuffle t-NN) (l-p w-. t-.))",
+        "(l-ROOT w-developed t-VBD (l-nsubj w-inc. t-NNP (l-rcmod w-founded t-VBD (l-dobj w-which t-WP) (l-nsubj w-jobs t-NNP)) (l-p w-, t-.)) (l-dobj w-shuffle t-NN) (l-p w-. t-.))",
         sent.getRootNode().toString());
   }
 
