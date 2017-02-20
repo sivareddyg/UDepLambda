cb4d9f3fbe00731ef5b8673d01bbcf43d25dcc96
Siva Reddy <siva@sivareddy.in>
2017-02-20 12:14:03 +0000

    data release

--- datarelease.mk
+++ datarelease.mk
@@ -9,3 +9,22 @@ webq_%:
 	python scripts/data-release.py \
 		< data/webquestions/$*/webquestions.test.json \
 		> webquestions-translations/$*.test.json
+
+sentences_webq_%:
+	mkdir -p working/webquestions-translations
+	python scripts/data-release-senteces.py \
+		< data/webquestions/$*/webquestions.train.json \
+		| sort -n \
+		| cut -f2 \
+		> working/webquestions-translations/$*.train.txt
+	python scripts/data-release-senteces.py \
+		< data/webquestions/$*/webquestions.dev.json \
+		| sort -n \
+		| cut -f2 \
+		> working/webquestions-translations/$*.dev.txt
+	python scripts/data-release-senteces.py \
+		< data/webquestions/$*/webquestions.test.json \
+		| sort -n \
+		| cut -f2 \
+		> working/webquestions-translations/$*.test.txt
+
